aacb7807c8cd9177d0b5f521efec6819e5042fa0
Ken Roberts <kroberts@9ci.com>
2016-07-20 16:40:10 -0500

    Updates to nine/tomcat

--- nine/tomcat/build.sh
+++ nine/tomcat/build.sh
@@ -6,4 +6,4 @@ fi
 WARNAME=`ssh teamcity.9ci.com 'ls -t ftp/wars/rcm/989/rcm-9.9.x* | head -n 1'` && \
 echo $WARNAME && \
 scp teamcity.9ci.com:$WARNAME rcm.war
-sudo docker build -t nine/tomcat:$1
+sudo docker build -t nine/tomcat:$1 .
